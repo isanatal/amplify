bf5d92dfc9954207869af3c48956e8b7a081a0ef
isanatal <al222310493@gmail.com>
2025-02-12 19:15:31 -0600

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Dayana mi amplify
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
