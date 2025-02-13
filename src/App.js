c43a72c0fcacd08082986ca4c3b6444e12791609
isanatal <al222310493@gmail.com>
2025-02-12 19:35:22 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Dayana mi amplify
+          Dayana mi amplify, QA
         </p>
       </header>
     </div>
